b0a0e6fb10d2683de65498807ecfa4c46d4d58e3
Kaden <kadenw258@gmail.com>
2024-09-27 18:22:24 -0500

    Completed daily page
    Completed the layout for the daily page

--- FitnessApp/src/Pages.jsx/Daily.jsx
+++ FitnessApp/src/Pages.jsx/Daily.jsx
@@ -3,13 +3,57 @@ import { useState } from "react";
 function Daily() {
   return (
     <div className="DailyPageContainer">
-      <h1>Daily Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam eum,
-        praesentium reprehenderit amet quas est earum quasi soluta perspiciatis,
-        ab aspernatur, tempore nisi inventore vero. Aliquid labore ullam error
-        odit.
-      </p>
+      <div className="InputContainer">
+        <input
+          className="WorkHeart"
+          type="text"
+          placeholder="Enter workout time"
+        ></input>
+        <button className="Submit">Save</button>
+
+        <input
+          className="WorkHeart"
+          type="text"
+          placeholder="Average resting heartrate"
+        ></input>
+        <button className="Submit">Save</button>
+      </div>
+
+      <div className="ZoneContainer">
+        <div className="Zone">
+          <button className="ZoneButton">Zone 1</button>
+          <h5>
+            When your heart beats at 50-60% of your maximum heart rate while
+            exercising for between 20–40 minutes
+          </h5>
+        </div>
+
+        <div className="Zone">
+          <button className="ZoneButton">Zone 1</button>
+          <h5>When your heart beats at 60-70% of your maximum heart rate</h5>
+        </div>
+
+        <div className="Zone">
+          <button className="ZoneButton">Zone 1</button>
+          <h5>
+            Exercising for 10–40 minutes with a heartbeat of 70-80% of your
+            maximum heart rate
+          </h5>
+        </div>
+
+        <div className="Zone">
+          <button className="ZoneButton">Zone 1</button>
+          <h5>
+            Exercising at 80-90% of your maximum heart rate for between 2–10
+            minutes
+          </h5>
+        </div>
+
+        <div className="Zone">
+          <button className="ZoneButton">Zone 1</button>
+          <h5>A heart rate at 90-100% of your maximum heart rate</h5>
+        </div>
+      </div>
     </div>
   );
 }
