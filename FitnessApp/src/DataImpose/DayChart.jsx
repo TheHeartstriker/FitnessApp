29edbc9272798894d12490f8d4392f6b1f89a32c
Kaden <kadenw258@gmail.com>
2024-12-18 13:35:48 -0600

    Data chart array mapping
    Mapped the data from the object into an array that we can impose with d3.js

--- FitnessApp/src/DataImpose/DayChart.jsx
+++ FitnessApp/src/DataImpose/DayChart.jsx
@@ -7,7 +7,7 @@ function DayChart({ dataprop, TimeProp }) {
   const svgWidth = "100%";
   const svgHeight = "100%";
   const [timeArr, setTimeArr] = useState([]);
-  function createTimeArr() {
+  async function createTimeArr() {
     const validFrames = {
       week: 7,
       month: 30,
@@ -21,15 +21,39 @@ function DayChart({ dataprop, TimeProp }) {
     }
   }
 
-  function fillTimeArr() {
-    let days = timeArr.length;
+  async function fillTimeArr() {
+    let days = 0;
+    //To track the date we are trying to find data for
     let currentDate = new Date();
-    while (days > 0) {
-      let timeActive = 0;
-      let currIterDate = data.find(
-        (item) => item.DataReconrded === currentDate.toDateString()
-      );
+    let tempTimeArr = Array(timeArr.length).fill(0);
+
+    for (let i = 0; i < timeArr.length; i++) {
+      //The date we are checking this increment
+      const recordDate = data[data.length - days - 1].DateRecorded;
+      //We check if the date we are checking is the same as the date in the data
+      console.log(currentDate.toISOString());
+      console.log(recordDate);
+      if (
+        //We remove the time from the date and compare
+        currentDate.toISOString().split("T")[0] === recordDate.split("T")[0]
+      ) {
+        let TempCal = 0;
+        TempCal += data[data.length - days - 1].Zone1Time * 4.5;
+        TempCal += data[data.length - days - 1].Zone2Time * 7.5;
+        TempCal += data[data.length - days - 1].Zone3Time * 11;
+        TempCal += data[data.length - days - 1].Zone4Time * 14.5;
+        TempCal += data[data.length - days - 1].Zone5Time * 16.5;
+        console.log(TempCal);
+        tempTimeArr[days] = TempCal;
+      } else {
+        // If no data is available for the day
+        tempTimeArr[days] = 0;
+      }
+      //Move to the day before
+      currentDate.setDate(currentDate.getDate() - 1);
+      days += 1;
     }
+    setTimeArr(tempTimeArr);
   }
 
   function drawBar(svg, x, y, width, height, color) {
@@ -57,13 +81,19 @@ function DayChart({ dataprop, TimeProp }) {
   }
 
   useEffect(() => {
-    console.log(data);
-    createTimeArr();
     MainCall();
     return () => {
       d3.select("#chart").select("svg").remove();
     };
-  }, []);
+  }, [timeArr]);
+
+  useEffect(() => {
+    createTimeArr().then(() => {
+      if (timeArr.length > 0) {
+        fillTimeArr();
+      }
+    });
+  }, [Time]);
 
   return <div id="chart" className="DayChart"></div>;
 }
