2b5847baa672465828146375cf115622cd2708f9
Kaden <kadenw258@gmail.com>
2024-12-21 14:57:06 -0600

    Finished the chart

--- FitnessApp/src/DataImpose/DayChart.jsx
+++ FitnessApp/src/DataImpose/DayChart.jsx
@@ -7,15 +7,16 @@ function DayChart({ dataprop, TimeProp }) {
   const svgHeight = "100%";
   const [data, setData] = useState([dataprop]);
   const [timeArr, setTimeArr] = useState([]);
+
   //Create a array of the length of the time frame in Timeprop
-  async function createTimeArr() {
+  function createTimeArr() {
     const validFrames = {
       week: 7,
       month: 30,
       year: 365,
     };
     const days = validFrames[Time.toLowerCase()];
-    if (days && !data.length < days) {
+    if (days) {
       setTimeArr(Array(days).fill(0));
     } else {
       console.error("Invalid Time Frame");
@@ -29,25 +30,17 @@ function DayChart({ dataprop, TimeProp }) {
   }
 
   //Starts at current and goes backwards to fill the time array
-  async function fillTimeArr() {
+  function fillTimeArr() {
     let dataIndex = 0;
     //To track the date we are trying to find data for
     let currentDate = new Date();
     let tempTimeArr = Array(timeArr.length).fill(0);
-
     for (let i = 0; i < tempTimeArr.length; i++) {
       //The date we are checking this increment
       let record = data[dataIndex];
       let recordDate = record
         ? new Date(record.DateRecorded).toISOString().split("T")[0]
         : null;
-
-      console.log(
-        recordDate,
-        "Is compared to",
-        currentDate.toISOString().split("T")[0]
-      );
-
       //We check if the date we are checking is the same as the date in the data
       if (
         //We remove the time from the date and compare
@@ -60,7 +53,6 @@ function DayChart({ dataprop, TimeProp }) {
         TempTime += data[dataIndex].Zone3Time;
         TempTime += data[dataIndex].Zone4Time;
         TempTime += data[dataIndex].Zone5Time;
-        console.log(TempTime);
         tempTimeArr[i] = TempTime;
         dataIndex += 1; // Move to the next record in data
       } else if (recordDate === null) {
@@ -72,25 +64,32 @@ function DayChart({ dataprop, TimeProp }) {
     }
     setTimeArr(tempTimeArr);
   }
-
-  function DrawDailyGraph(svg, pixelH) {
+  //Draws into the graph based on the data in timeArr
+  function DrawDailyGraph(svg, pixelH, pixelW) {
     const reversedTemp = [...timeArr].reverse();
     let xVal = 0;
-    const gap = 10; // Define the gap between bars
+    let gap = 5;
+    if (timeArr.length > 100) {
+      gap = 0;
+    }
+    const barWidth = pixelW / timeArr.length - gap;
     for (let i = 0; i < reversedTemp.length; i++) {
-      const barHeight = reversedTemp[i] * 3 + 30; // Adjust height based on reversedTemp[i]
+      const barHeight = reversedTemp[i] * 3;
       drawBar(
         svg,
+        // Positioning of the bar
         xVal,
-        pixelH - barHeight, // Position at the bottom
-        50, // Width of the bar
+        pixelH - barHeight,
+        // Width
+        barWidth,
+        // Height
         barHeight,
         "blue"
       );
-      xVal += 50 + gap; // Add gap between bars
+      xVal += barWidth + gap;
     }
   }
-
+  //Helper function to draw the bars
   function drawBar(svg, x, y, width, height, color) {
     svg
       .append("rect")
@@ -112,9 +111,20 @@ function DayChart({ dataprop, TimeProp }) {
     }
 
     const PixelHeight = svgPointer.node().getBoundingClientRect().height;
-
-    DrawDailyGraph(svgPointer, PixelHeight);
+    const PixelWidth = svgPointer.node().getBoundingClientRect().width;
+    DrawDailyGraph(svgPointer, PixelHeight, PixelWidth);
   }
+
+  useEffect(() => {
+    sortByDate(dataprop);
+    createTimeArr();
+  }, [Time]);
+
+  useEffect(() => {
+    if (timeArr.length > 0) {
+      fillTimeArr();
+    }
+  }, [timeArr.length]);
 
   useEffect(() => {
     MainCall();
@@ -122,18 +132,6 @@ function DayChart({ dataprop, TimeProp }) {
       d3.select("#chart").select("svg").remove();
     };
   }, [timeArr, Time]);
-
-  useEffect(() => {
-    sortByDate(dataprop);
-    createTimeArr().then(() => {
-      if (timeArr.length > 0) {
-        fillTimeArr();
-        console.log(timeArr);
-        console.log(data);
-      }
-    });
-  }, [Time]);
-
   return <div id="chart" className="DayChart"></div>;
 }
 
