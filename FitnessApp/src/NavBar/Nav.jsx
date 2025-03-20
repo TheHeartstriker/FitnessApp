d6593b65bff0145ba508b2ba0d4e3662a2d72752
Kaden <kadenw258@gmail.com>
2025-03-20 12:56:33 -0500

    Linking
    Added the proper path linking, naming and some other small touch ups

--- FitnessApp/src/NavBar/Nav.jsx
+++ FitnessApp/src/NavBar/Nav.jsx
@@ -19,28 +19,54 @@ function Nav() {
     setIsCollapsed(!isCollapsed);
   };
 
+  function GithubLink() {
+    window.open(
+      "https://github.com/TheHeartstriker/FitnessApp",
+      "_blank",
+      "noopener noreferrer"
+    );
+  }
+
   return (
     <div className="NavContainer">
       <div className="Ball"></div>
       <div className="NavContent">
-        <div className="NavIcon">
+        <div
+          className={`NavIcon ${
+            location.pathname === "/login" ? "Active" : ""
+          }`}
+          onClick={() => navigate("/login")}
+        >
           <ArrowLeftEndOnRectangleIcon className="Icon"></ArrowLeftEndOnRectangleIcon>
           <h1>Login</h1>
         </div>
-        <div className="NavIcon">
+        <div
+          className={`NavIcon ${
+            location.pathname === "/daily" ? "Active" : ""
+          }`}
+          onClick={() => navigate("/daily")}
+        >
           <HeartIcon className="Icon"></HeartIcon>
-          <h1>Login</h1>
+          <h1>Daily</h1>
         </div>
-        <div className="NavIcon">
+        <div
+          className={`NavIcon ${location.pathname === "/view" ? "Active" : ""}`}
+          onClick={() => navigate("/view")}
+        >
           <AdjustmentsHorizontalIcon className="Icon"></AdjustmentsHorizontalIcon>
-          <h1>Login</h1>
+          <h1>Stats</h1>
         </div>
-        <div className="NavIcon">
+        <div
+          className={`NavIcon ${
+            location.pathname === "/share" ? "Active" : ""
+          }`}
+          onClick={() => navigate("/share")}
+        >
           <ShareIcon className="Icon"></ShareIcon>
-          <h1>Login</h1>
+          <h1>Share</h1>
         </div>
       </div>
-      <div className="GithubIcon"></div>
+      <div className="GithubIcon" onClick={() => GithubLink()}></div>
     </div>
   );
 }
