97319050ea559ae3adf4b0700e7bd15433b8bd29
Kaden <kadenw258@gmail.com>
2025-10-09 03:05:04 -0500

    Nav bar somewhat complete
    The nav bar is sort of done. Still have work todo but its close to compeltion.

--- FitnessApp/src/Components/NavBar/Nav.jsx
+++ FitnessApp/src/Components/NavBar/Nav.jsx
@@ -1,12 +1,28 @@
 import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Logo from "../../assets/icons/logo.jsx";
 import "./Nav.css";
 import CubeIcon from "../../assets/icons/cube.jsx";
 import HeartIcon from "../../assets/icons/heart.jsx";
 import PeopleIcon from "../../assets/icons/people.jsx";
 function Nav() {
+  //Position the bar indicator needs to move to reach different buttons
+  let indicatorPositions = {
+    "/view": "0px",
+    "/daily": "60px",
+    "/share": "120px",
+  };
+
   const navigate = useNavigate();
   const location = useLocation();
+  const [indicatorTop, setIndicatorTop] = useState(
+    indicatorPositions[location.pathname] || "0px"
+  );
+
+  useEffect(() => {
+    setIndicatorTop(indicatorPositions[location.pathname] || "0px");
+  }, [location.pathname]);
+
   return (
     <nav className="nav-bar">
       <div className="nav-logo-container">
@@ -15,16 +31,37 @@ function Nav() {
         </div>
         <h1>FGraphs</h1>
       </div>
-      <button className="nav-bar-button" id="test-active">
-        <div className="on-bar-indicator"></div>
+      {/* Route buttons */}
+      {/* Button 1 */}
+      <button
+        className={`nav-bar-button ${
+          location.pathname === "/view" ? "active" : ""
+        }`}
+        id="test-active"
+        onClick={() => navigate("/view")}
+      >
+        <div className="on-bar-indicator" style={{ top: indicatorTop }}></div>
         <CubeIcon />
         <h2>Dashboard</h2>
       </button>
-      <button className="nav-bar-button">
+      {/* Button 2 */}
+      <button
+        className={`nav-bar-button ${
+          location.pathname === "/daily" ? "active" : ""
+        }`}
+        onClick={() => navigate("/daily")}
+      >
         <HeartIcon />
         <h2>Daily</h2>
       </button>
-      <button className="nav-bar-button" id="share-button">
+      {/* Button 3 */}
+      <button
+        className={`nav-bar-button ${
+          location.pathname === "/share" ? "activeShare" : ""
+        }`}
+        id="share-button"
+        onClick={() => navigate("/share")}
+      >
         <PeopleIcon />
         <h2>Share</h2>
       </button>
