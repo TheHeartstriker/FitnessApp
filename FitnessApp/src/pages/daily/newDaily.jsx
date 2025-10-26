fb4af31fff08423646a3382e5b1ad45ce448f7f4
Kaden <kadenw258@gmail.com>
2025-10-25 22:56:48 -0500

    Daily is now functionality done

--- FitnessApp/src/pages/daily/newDaily.jsx
+++ FitnessApp/src/pages/daily/newDaily.jsx
@@ -3,9 +3,37 @@ import "./newDaily.css";
 import Weight from "../../assets/icons2/weight.jsx";
 import HeartFill from "../../assets/icons2/heartFill.jsx";
 import Mug from "../../assets/icons2/mug.jsx";
+import { formatDateToMySQL } from "../../utils/funcUtil.jsx";
+import { useRef } from "react";
+import { saveData } from "../../services/apiFitness.jsx";
 import { underStandingZonesText, loggingInstructionsText } from "./text.js";
 function NewDaily() {
   const [zone, setZone] = useState(1);
+  const workoutTimeRef = useRef(null);
+  const restingHeartRateRef = useRef(null);
+  const weightRef = useRef(null);
+  const currentDate = formatDateToMySQL(new Date());
+
+  async function submit(dbName, value, date) {
+    try {
+      await saveData(dbName, value, date);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function stringName(zone) {
+    return `Zone${zone}Time`;
+  }
+
+  function setNumber(ref, value, e) {
+    if (!isNaN(value) && value.length <= 4) {
+      ref.current.value = value;
+    } else {
+      ref.current.value = "";
+    }
+    return;
+  }
 
   return (
     <div className="daily">
@@ -45,10 +73,22 @@ function NewDaily() {
             <Weight />
             <div className="daily-input">
               <h3>Daily weight</h3>
-              <input type="text" placeholder="Enter your weight!" />
+              <input
+                ref={weightRef}
+                type="text"
+                placeholder="Enter your weight!"
+                onChange={(e) => setNumber(weightRef, e.target.value)}
+              />
             </div>
           </div>
-          <button className="daily-submit-button">Submit</button>
+          <button
+            className="daily-submit-button"
+            onClick={() =>
+              submit("weight", weightRef.current.value, currentDate)
+            }
+          >
+            <h4>Submit</h4>
+          </button>
         </div>
         {/*  */}
         {/* Input for heart rate */}
@@ -57,10 +97,26 @@ function NewDaily() {
             <HeartFill />
             <div className="daily-input">
               <h3>Average heartrate</h3>
-              <input type="text" placeholder="Enter your heart rate!" />
+              <input
+                ref={restingHeartRateRef}
+                type="text"
+                placeholder="Enter your heart rate!"
+                onChange={(e) => setNumber(restingHeartRateRef, e.target.value)}
+              />
             </div>
           </div>
-          <button className="daily-submit-button">Submit</button>
+          <button
+            className="daily-submit-button"
+            onClick={() =>
+              submit(
+                "resting_heart",
+                restingHeartRateRef.current.value,
+                currentDate
+              )
+            }
+          >
+            <h4>Submit</h4>
+          </button>
         </div>
         {/*  */}
         {/* Input for hydration */}
@@ -69,10 +125,26 @@ function NewDaily() {
             <Mug />
             <div className="daily-input">
               <h3>Workout time</h3>
-              <input type="text" placeholder="Enter your time spent in" />
+              <input
+                ref={workoutTimeRef}
+                type="text"
+                placeholder="Enter your time spent in"
+                onChange={(e) => setNumber(workoutTimeRef, e.target.value)}
+              />
             </div>
           </div>
-          <button className="daily-submit-button">Submit</button>
+          <button
+            className="daily-submit-button"
+            onClick={() =>
+              submit(
+                stringName(zone),
+                workoutTimeRef.current.value,
+                currentDate
+              )
+            }
+          >
+            <h4>Submit</h4>
+          </button>
         </div>
       </section>
     </div>
