8f2b8d36c1e09bee94f2b1275df8133919483571
Kaden <kadenw258@gmail.com>
2025-11-17 16:21:18 -0600

    card basic
    The cards are made and are generally what they will look like when its done. Still have quite a bit todo.

--- FitnessApp/src/pages/landing/featured/featured.jsx
+++ FitnessApp/src/pages/landing/featured/featured.jsx
@@ -6,12 +6,13 @@ function Featured() {
     <section className="featured-section">
       <Ellipse />
       {/* Card one share */}
-      <div className="featured-card">
+      <div className="featured-card featured-card--left">
         <div className="featured-card-image">
           <img src="/src/assets/landing/connection.png" alt="Sharing Image" />
         </div>
         <div className="featured-card-text">
           <div className="featured-card-text-pill">
+            <span></span>
             <h3>Feature 1</h3>
           </div>
           <h4>Integrated data sharing system</h4>
@@ -23,12 +24,13 @@ function Featured() {
         </div>
       </div>
       {/* Card two data analysis */}
-      <div className="featured-card">
+      <div className="featured-card featured-card--right">
         <div className="featured-card-image">
           <img src="/src/assets/landing/graphs.png" alt="Analytics Image" />
         </div>
         <div className="featured-card-text">
-          <div className="featured-card-text-pill">
+          <div className="featured-card-text-pill featured-card-text-pill--second">
+            <span></span>
             <h3>Feature 1</h3>
           </div>
           <h4>Integrated data sharing system</h4>
@@ -40,12 +42,13 @@ function Featured() {
         </div>
       </div>
       {/* Card three data design? */}
-      <div className="featured-card">
+      <div className="featured-card featured-card--left">
         <div className="featured-card-image">
           <img src="/src/assets/landing/design.png" alt="Design Image" />
         </div>
         <div className="featured-card-text">
-          <div className="featured-card-text-pill">
+          <div className="featured-card-text-pill featured-card-text-pill--third">
+            <span></span>
             <h3>Feature 1</h3>
           </div>
           <h4>Integrated data sharing system</h4>
