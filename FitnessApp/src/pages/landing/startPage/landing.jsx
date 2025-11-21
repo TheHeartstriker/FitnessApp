90a619c8071c05e4ff3fa596c815f03a121728da
Kaden <kadenw258@gmail.com>
2025-11-20 18:21:07 -0600

    Mobile and new hero section.

--- FitnessApp/src/pages/landing/startPage/landing.jsx
+++ FitnessApp/src/pages/landing/startPage/landing.jsx
@@ -12,7 +12,9 @@ function LandingPage() {
       "h1#landing-char, h2#landing-char, h3#landing-char"
     );
     const pTargets = document.querySelectorAll("p#landing-char");
-    const landingButton = document.querySelectorAll(".primary-action-btn");
+    const landingButton = document.querySelectorAll(
+      ".primary-action-btn, #button-char"
+    );
 
     // Animate h tags with faster stagger
     hTargets.forEach((target) => {
@@ -23,7 +25,7 @@ function LandingPage() {
         y: [{ to: ["100%", "0%"] }],
         opacity: [0, 1],
         ease: "cubicBezier(0.7, 0.1,0.5,0.9)",
-        delay: stagger(50, { start: 750 }), // Faster stagger for h tags
+        delay: stagger(75, { start: 750 }), // Faster stagger for h tags
       });
     });
 
@@ -36,13 +38,13 @@ function LandingPage() {
         y: [{ to: ["100%", "0%"] }],
         opacity: [0, 1],
         ease: "cubicBezier(0.7, 0.1,0.5,0.9)",
-        delay: stagger(15, { start: 750 }), // Slower stagger for p tag
+        delay: stagger(15, { start: 800 }), // Slower stagger for p tag
       });
     });
     animate(landingButton, {
       translateY: [{ from: 20, to: 0 }],
       opacity: [0, 1],
-      duration: 500,
+      duration: 650,
       ease: "cubicBezier(0.7, 0.1,0.5,0.9)",
       delay: 2750,
     });
@@ -57,18 +59,29 @@ function LandingPage() {
           <TriAngleBackgroundAni />
         </div>
         <div className="landing-content">
-          <h2 id="landing-char">THIS IS</h2>
           <h1 id="landing-char">FGraphs</h1>
-          <h3 id="landing-char">A simplistic take on fitness tracking</h3>
+          <h2 id="landing-char">A simplistic take on fitness tracking</h2>
           <p id="landing-char">
             Hi there! FGraphs is a fitness tracking app! Mainly made in my spare
             time to learn, have fun and showcase my skills and understanding of
             web development. But! Its also a pretty neat app to track your
             fitness if you into that.
           </p>
-          <ScrollDown percent={100} className="primary-action-btn">
-            Want to give it a try?
-          </ScrollDown>
+          <div className="landing-button-section">
+            {/* btn1 */}
+            <ScrollDown percent={100} className="primary-action-btn">
+              Want to give it a try?
+            </ScrollDown>
+            {/* separator */}
+            <hr id="button-char"></hr>
+            {/*  */}
+            {/* btn2 */}
+            <ScrollDown percent={100} className="primary-action-btn circle-btn">
+              K
+            </ScrollDown>
+            {/* Text */}
+            <h3 id="button-char">More from me</h3>
+          </div>
         </div>
       </section>
       <Featured />
