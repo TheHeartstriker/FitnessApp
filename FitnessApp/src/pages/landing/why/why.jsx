ac9577f08011a8e8bdf0f2802f2dec2b4aacc9b2
Kaden <kadenw258@gmail.com>
2025-11-22 15:42:51 -0600

    Why animation
    Got the why animation in a good spot going to try and improve it before adding scroll.

--- FitnessApp/src/pages/landing/why/why.jsx
+++ FitnessApp/src/pages/landing/why/why.jsx
@@ -1,5 +1,52 @@
 import "./why.css";
+import { useEffect } from "react";
+import { animate, splitText, stagger } from "animejs";
 function Why() {
+  function animateWhySection() {
+    const { words: Words1 } = splitText(".why-section-header h3", {
+      words: { wrap: "clip" },
+    });
+    const { words: Words2 } = splitText(".why-section-story h4", {
+      words: { wrap: "clip" },
+    });
+    const words = [...Words1, ...Words2];
+    const { words: pWords1 } = splitText("#why-p-text-1", {
+      words: { wrap: "clip" },
+    });
+    const { words: pWords2 } = splitText("#why-p-text-2", {
+      words: { wrap: "clip" },
+    });
+    const pWords = [...pWords1, ...pWords2];
+    pWords.forEach((word) => {
+      word.style.opacity = 0;
+    });
+
+    // Calculate the total duration including stagger
+    const staggerDelay = 50;
+    const startDelay = 750;
+    const wordsDuration = startDelay + words.length * staggerDelay + 500;
+
+    animate(words, {
+      y: [{ to: ["100%", "0%"] }],
+      opacity: [0, 1],
+      ease: "out(3)",
+      delay: stagger(staggerDelay, { start: startDelay }),
+    });
+    // Start pWords animation at the calculated time
+    animate(pWords, {
+      y: [{ to: ["100%", "0%"] }],
+      opacity: [0, 1],
+      translateY: [{ from: 30, to: 0 }],
+      duration: 1250,
+      ease: "out(3)",
+      delay: wordsDuration,
+    });
+  }
+
+  useEffect(() => {
+    animateWhySection();
+  }, []);
+
   return (
     <section className="why-section">
       <div className="why-section-header">
@@ -7,7 +54,7 @@ function Why() {
       </div>
       <div className="why-section-story">
         <h4>The story</h4>
-        <p>
+        <p id="why-p-text-1">
           This site exists for a variety of reasons. The main one is that I, the
           dev, Kaden Wildauer, want to prove and improve my skill in full-stack
           web development. So I needed something simple to get my hands on, but
@@ -16,7 +63,7 @@ function Why() {
           is so important to me and that I spend thousands of hours doing. If I
           am, please come roast me at my email: serkadenwildauer@gmail.com.
         </p>
-        <p>
+        <p id="why-p-text-2">
           Another large reason is that I enjoy fitness! I am a calisthenics guy.
           But I hate fitness apps usually they are bulky and require too much
           time and effort from me. I just want quick, semi accurate readings on
